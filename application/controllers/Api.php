c54617b589e99bfacaf93d4d0b5686286dbc5b9c
Indra Gunanda <indra.gunanda@gmail.com>
2018-07-04 05:15:07 +0700

    BOT [MAIN SYSTEM]

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -61,11 +61,268 @@ class Api extends REST_Controller
  	 * Memuat data Tracking Mobil
  	 * @return json
 	 */
-   public function test_get()
+   public function run_get()
    {
      $this->load->model("proses/bot");
-     $d = $this->bot->ardortrade("asksell","7","0.5","SECRET");
+     $this->main->setTable("botrules");
+     $rules = $this->main->get();
+     $data = [];
+     foreach ($rules->result() as $key => $value) {
+       $this->main->setTable("ardor");
+       $getacc = $this->main->get();
+       $limit = $getacc->num_rows();
+       $random = rand(0,($limit-1));
+       $getaccarray = $getacc->result();
+       $ds = $this->bot->getpercentagecoin($value->spread,$getaccarray[$random]->publickey,$value->coin);
+       $res = $ds;
+       $data[] = $res;
+       for ($i=0; $i <= 1 ; $i++) {
+         if ($i == 0) {
+           $tipe = "askbuy";
+           $d = $this->bot->ardortrade($tipe,$res["buy"],$res["buyrate"],$getaccarray[$random]->secretkey);
+         }else {
+           $tipe = "asksell";
+           $d = $this->bot->ardortrade($tipe,$res["sell"],$res["sellrate"],$getaccarray[$random]->secretkey);
+         }
+         if ($d != FALSE) {
+           $data = [];
+           $data["id_ardor"] = $getaccarray[$random]->id_ardor;
+           $data["tipe"] = $tipe;
+           $data["quantityQNT"] = $d->transactionJSON->attachment->quantityQNT;
+           $data["priceNQTPerShare"] = $d->transactionJSON->attachment->priceNQTPerShare;
+           $data["fullhash"] = $d->transactionJSON->fullHash;
+           $this->main->setTable("ardor_botrecord");
+           $ins = $this->main->insert($data);
+           if (!$ins) {
+             $this->main->setTable("log_failed");
+             $this->main->insert(["publickey"=>$getaccarray[$random]->publickey,"reason"=>"Failed Insert"]);
+             break;
+           }
+         }else {
+           $this->main->setTable("log_failed");
+           $this->main->insert(["publickey"=>$getaccarray[$random]->publickey,"reason"=>"Out Of Gas"]);
+           break;
+         }
+       }
+     }
+
+   }
+   public function lastrade_get()
+   {
+     $this->load->model("proses/bot");
+     $d = $this->bot->getlastrade();
+     $this->response($d);
+   }
+   public function getsaldo_get($getsaldo = "")
+   {
+     $this->load->model("proses/bot");
+     $d = $this->bot->ceksaldo($getsaldo);
      $this->response($d);
    }
 
+   public function isadmin()
+   {
+     if ($this->session->id_admin == NULL) {
+       $this->response(["status"=>0,"msg"=>"Session Expired"]);
+     }
+   }
+   public function login_post()
+   {
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("admin");
+     $dpost["password"] = md5($dpost["password"]);
+     $get = $this->main->get($dpost);
+     if ($get->num_rows() > 0) {
+       $this->session->set_userdata($get->row());
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function admininsert_post()
+   {
+     $this->isadmin();
+     $dpost = $this->main->post(null,true);
+     $this->main->setTable("admin");
+     $ins = $this->main->insert($dpost);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function adminread_get($tipe='')
+   {
+     $this->isadmin();
+     $this->main->setTable("admin");
+     $get = $this->main->get();
+     if ($tipe == "selectize") {
+       $this->response($get->result());
+     }else {
+       $data = [];
+       $data["data"] = [];
+       foreach ($get->result() as $key => $value) {
+         $data["data"][] = [$value->id_admin,$value->email,$value->username];
+       }
+       $this->response($data);
+     }
+   }
+   public function admindelete_post()
+   {
+     $this->isadmin();
+     $dpost = $this->main->post(null,true);
+     $this->main->setTable("admin");
+     $ins = $this->main->delete($dpost);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   // Tabel Ardor
+   public function ardorread_get($tipe='')
+   {
+     $this->isadmin();
+     $this->main->setTable("ardor");
+     $get = $this->main->get();
+     if ($tipe == "selectize") {
+       $this->response($get->result());
+     }else {
+       $data = [];
+       $data["data"] = [];
+       foreach ($get->result() as $key => $value) {
+         $d = $this->bot->ceksaldo($value->publickey);
+         $data["data"][] = [$value->id_ardor,$value->publickey,$d["fras"],$d["ignis"],$value->date_time];
+       }
+       $this->response($data);
+     }
+   }
+   public function ardorinsert_post()
+   {
+     $this->isadmin();
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("ardor");
+     $ins = $this->main->insert($dpost);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function ardorupdate_post()
+   {
+     $this->isadmin();
+     $dpost = $this->input->post(null,true);
+     $id = ["id_ardor"=>$dpost['id_ardor']];
+     unset($dpost['id_ardor']);
+     $this->main->setTable("ardor");
+     $ins = $this->main->update($dpost,$id);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function ardordelete_post()
+   {
+     $this->isadmin();
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("ardor");
+     $ins = $this->main->delete($dpost);
+       $this->response(["status"=>1]);
+       if ($ins) {
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   // Ardor Bot Record
+   public function ardorrecord_get($tipe='')
+   {
+     $this->isadmin();
+     $this->main->setTable("ardor_botrecord");
+     $get = $this->main->get();
+     if ($tipe == "selectize") {
+       $this->response($get->result());
+     }else {
+       $data = [];
+       $data["data"] = [];
+       foreach ($get->result() as $key => $value) {
+         $this->main->setTable("ardor");
+         $d = $this->main->get(["id_ardor"=>$value->id_ardor]);
+         $value->publickey = $d->row()->publickey;
+         $data["data"][] = [$value->id_ardor_botrecord,$value->tipe,$value->publickey,$value->status_callback,($value->quantityQNT/100000000),($value->priceNQTPerShare/100000000),$value->fullhash];
+       }
+       $this->response($data);
+     }
+   }
+   // Bot Rules
+   public function botrulesinsert_post()
+   {
+     $this->isadmin();
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("botrules");
+     $ins = $this->main->insert($dpost);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function botrulesread_get($tipe='')
+   {
+     $this->isadmin();
+     $this->main->setTable("botrules");
+     $get = $this->main->get();
+     if ($tipe == "selectize") {
+       $this->response($get->result());
+     }else {
+       $data = [];
+       $data["data"] = [];
+       foreach ($get->result() as $key => $value) {
+         $data["data"][] = [$value->id_botrules,$value->spread,$value->coin,$value->created];
+       }
+       $this->response($data);
+     }
+   }
+   public function botrulesupdate_post()
+   {
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("botrules");
+     $id = ["id_botrules"=>$dpost["id_botrules"]];
+     unset($dpost["id_botrules"]);
+     $ins = $this->main->update($dpost,$id);
+     if ($ins) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   public function botrulesdelete_post()
+   {
+     $dpost = $this->input->post(null,true);
+     $this->main->setTable("botrules");
+     $del = $this->main->delete($dpost);
+     if ($del) {
+       $this->response(["status"=>1]);
+     }else {
+       $this->response(["status"=>0]);
+     }
+   }
+   // Log Failed
+   public function logfailedread_get($tipe='')
+   {
+     $this->isadmin();
+     $this->main->setTable("log_failed");
+     $get = $this->main->get();
+     if ($tipe == "selectize") {
+       $this->response($get->result());
+     }else {
+       $data = [];
+       $data["data"] = [];
+       foreach ($get->result() as $key => $value) {
+         $data["data"][] = [$value->id_log_failed,$value->publickey,$value->created];
+       }
+       $this->response($data);
+     }
+   }
 }
--- application/models/proses/Bot.php
+++ application/models/proses/Bot.php
@@ -3,11 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bot extends CI_Model{
   public $frasid;
+  public $crowdsale_price = 2.48;
   public function __construct()
   {
     parent::__construct();
     $this->load->model("proses/ardor");
     $this->frasid = "4777913785555377445";
+
   }
   public function ardortrade($type='',$qty='',$rate='',$secret="")
   {
@@ -30,6 +32,75 @@ class Bot extends CI_Model{
       $res = $this->ardor->post("placeAskOrder",["chain"=>2,"asset"=>$this->frasid,"quantityQNT"=>$qty*100000000,"priceNQTPerShare"=>$rate*100000000,"secretPhrase"=>$secret,"feeNQT"=>$fee]);
       return $res;
     }
+  }
+  public function getlastrade()
+  {
+      $last = $this->ardor->post("getLastTrades",["chain"=>2,"assets"=>$this->frasid]);
+      if (isset($last->trades)) {
+        return $last;
+      }else {
+        return false;
+      }
+  }
+  public function ceksaldo($acc='')
+  {
+    $last = $this->ardor->post("getBalances",["chain"=>2,"account"=>$acc]);
+    $alldata = [];
+    if (isset($last->balances)) {
+      $saldoNative = 0;
+      foreach($last->balances as $s => $a){
+        if(isset($a->balanceNQT)){
+          $saldoNative = $a->balanceNQT;
+          break;
+        }
+      }
+      $saldoNative = $saldoNative / 100000000;
+    }else {
+      return false;
+    }
+    $last = $this->ardor->post("getAccountAssets",["asset"=>$this->frasid,"account"=>$acc]);
+    $saldo = 0;
+    if (isset($last->quantityQNT)) {
+      $saldo = $last->quantityQNT;
+      $saldo = $saldo / 100000000;
+    }else {
+      return false;
+    }
+    return ["fras"=>$saldo,"ignis"=>$saldoNative];
+  }
+  public function getpercentagecoin($spread="",$acc='',$percent='')
+  {
+      $last = $this->ardor->post("getBalances",["chain"=>2,"account"=>$acc]);
+      $alldata = [];
+      if (isset($last->balances)) {
+        $saldoNative = 0;
+        foreach($last->balances as $s => $a){
+          if(isset($a->balanceNQT)){
+            $saldoNative = $a->balanceNQT;
+            break;
+          }
+        }
+        $saldoNative = $saldoNative / 100000000;
+        $saldoNative = $saldoNative*($percent/100);
+      }else {
+        return false;
+      }
+      $last = $this->ardor->post("getAccountAssets",["asset"=>$this->frasid,"account"=>$acc]);
+      $saldo = 0;
+      if (isset($last->quantityQNT)) {
+        $saldo = $last->quantityQNT;
+        $saldo = $saldo / 100000000;
+        $saldo = $saldo*($percent/100);
+      }else {
+        return false;
+      }
+      $lget = $this->getlastrade();
+      if ($lget != FALSE) {
+        $this->crowdsale_price = ($lget->trades[0]->priceNQTPerShare/100000000);
+        return ["sell"=>substr($saldo,0,5),"sellrate"=>substr($this->crowdsale_price+($this->crowdsale_price*(($spread/2)/100)),0,5),"buy"=>substr($saldoNative,0,5),"buyrate"=>substr($this->crowdsale_price - ($this->crowdsale_price*(($spread/2)/100)),0,5)];
+      }else {
+        return false;
+      }
   }
 
 }
